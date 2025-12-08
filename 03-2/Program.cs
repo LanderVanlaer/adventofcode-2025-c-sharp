ff888d9419638fc444031924945085d8143b6b89
Lander Van laer <lander.vanlaer1@gmail.com>
2025-12-08 22:54:30 +0100

    feat(03): Day 3 - Part 2 - Lobby

---
+++ 03-2/Program.cs
@@ -0,0 +1,51 @@
+﻿using StreamReader file = new(args[0]);
+
+const int size = 12;
+
+long totalJoltage = 0;
+
+while (await file.ReadLineAsync() is { } bank)
+{
+	Index[] indices = FindHighestJoltage(bank.AsSpan(), size);
+
+	for (int i = 0; i < size; ++i)
+	{
+		totalJoltage += (bank[indices[i]] - '0') * (long)Math.Pow(10, size - i - 1);
+	}
+}
+
+Console.WriteLine(totalJoltage);
+
+return 0;
+
+static Index[] FindHighestJoltage(ReadOnlySpan<char> bank, int total)
+{
+	Index[] indices = new Index[total];
+	int startFrom = 0;
+
+	for (int i = 0; i < total; ++i)
+	{
+		int maxIndex = total - i - 1;
+
+		int index = startFrom + IndexOfHighestValue(bank[startFrom..^maxIndex]);
+		indices[i] = index;
+		startFrom = index + 1;
+	}
+
+	return indices;
+}
+
+static int IndexOfHighestValue(ReadOnlySpan<char> list)
+{
+	int index = 0;
+
+	for (int i = 1; i < list.Length; ++i)
+	{
+		if (list[i] > list[index])
+		{
+			index = i;
+		}
+	}
+
+	return index;
+}
