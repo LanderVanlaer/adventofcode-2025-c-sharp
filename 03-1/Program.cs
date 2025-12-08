6416c2f907a5f6fb0212db15eb59c5d5414b3557
Lander Van laer <lander.vanlaer1@gmail.com>
2025-12-08 22:35:14 +0100

    feat(03): Day 3 - Part 1 - Lobby

---
+++ 03-1/Program.cs
@@ -0,0 +1,41 @@
+﻿using StreamReader file = new(args[0]);
+
+long totalJoltage = 0;
+
+while (await file.ReadLineAsync() is { } bank)
+{
+	(Index firstBattery, Index lastBattery) = FindHighestJoltage(bank.AsSpan());
+
+	int joltageOfBank = (bank[firstBattery] - '0') * 10 + (bank[lastBattery] - '0');
+
+	totalJoltage += joltageOfBank;
+}
+
+Console.WriteLine(totalJoltage);
+
+return 0;
+
+(Index firstBattery, Index lastBattery) FindHighestJoltage(ReadOnlySpan<char> bank)
+{
+	int firstBatteryIndex = 0;
+
+	for (int i = 1; i < bank.Length - 1; i++)
+	{
+		if (bank[i] > bank[firstBatteryIndex])
+		{
+			firstBatteryIndex = i;
+		}
+	}
+
+	int lastBatteryIndex = firstBatteryIndex + 1;
+
+	for (int i = lastBatteryIndex + 1; i < bank.Length; i++)
+	{
+		if (bank[i] > bank[lastBatteryIndex])
+		{
+			lastBatteryIndex = i;
+		}
+	}
+
+	return (firstBatteryIndex, lastBatteryIndex);
+}
