8e3bbdd2ea459663979c8f231421a80e9a8fc184
Lander Van laer <lander.vanlaer1@gmail.com>
2025-12-12 21:45:31 +0100

    feat(07): Day 7 - Part 1 - Laboratories

---
+++ 07-1/Program.cs
@@ -0,0 +1,50 @@
+﻿using StreamReader file = new(args[0]);
+
+string firstLine = await file.ReadLineAsync() ?? throw new InvalidOperationException("Input file is empty");
+int length = firstLine.Length;
+
+Span<char> prev = stackalloc char[length];
+Span<char> curr = stackalloc char[length];
+
+firstLine.CopyTo(prev);
+
+prev[prev.IndexOf('S')] = '|';
+
+int count = 0;
+
+while (file.ReadLine() is { } line)
+{
+	line.CopyTo(curr);
+
+	for (int i = 0; i < length; ++i)
+	{
+		if (prev[i] != '|')
+		{
+			continue;
+		}
+
+		switch (curr[i])
+		{
+			case '.':
+				curr[i] = '|';
+				break;
+			case '^':
+				++count;
+				if (i != 0)
+				{
+					curr[i - 1] = '|';
+				}
+
+				if (i != length - 1)
+				{
+					curr[i + 1] = '|';
+				}
+
+				break;
+		}
+	}
+
+	curr.CopyTo(prev);
+}
+
+Console.WriteLine(count);
