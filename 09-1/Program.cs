df19aeec8ec1c9d2ec57bef9cc14e4ed2964f8a8
Lander Van laer <lander.vanlaer1@gmail.com>
2025-12-14 00:31:12 +0100

    feat(09): Day 9 - Part 1 - Movie Theater

---
+++ 09-1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Numerics;
+
+using StreamReader file = new(args[0]);
+
+List<Vector2> points = [];
+
+while (await file.ReadLineAsync() is { } line)
+{
+	int firstComma = line.IndexOf(',');
+	int x = int.Parse(line[..firstComma]);
+	int y = int.Parse(line[(firstComma + 1)..]);
+	points.Add(new Vector2(x, y));
+}
+
+double biggestArea = 0;
+
+for (int i = 0; i < points.Count; ++i)
+{
+	for (int j = i + 1; j < points.Count; ++j)
+	{
+		double area =
+			(Math.Abs((double)points[i].X - points[j].X) + 1) * (Math.Abs((double)points[i].Y - points[j].Y) + 1);
+
+		biggestArea = Math.Max(area, biggestArea);
+	}
+}
+
+Console.WriteLine(biggestArea.ToString("0"));
